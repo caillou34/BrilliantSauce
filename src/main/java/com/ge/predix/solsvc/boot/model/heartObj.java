73469ee8245547e76f280b0502d19959ee3f2a10
Eric Blackmon <eric.blackmon@ge.com>
2017-07-18 09:58:40 -0500

    Actuallly added heart and temp object

---
+++ src/main/java/com/ge/predix/solsvc/boot/model/heartObj.java
@@ -0,0 +1,19 @@
+package com.ge.predix.solsvc.boot.model;
+
+/**
+ * Created by ericblackmon on 7/14/17.
+ */
+public class heartObj {
+
+    private static double heart;
+
+    public static double getHeart(){
+
+        return heart;
+    }
+
+    public static void setHeart(double hrt){
+
+        heart= hrt;
+    }
+}
